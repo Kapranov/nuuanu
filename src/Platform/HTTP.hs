47ad08de810a8eb79f8ea49ab30e2dd963e7aad7
Oleg G.Kapranov <ok@pure-agency.co>
2024-10-20 01:34:19 -1000

    added matchesId for url parms

--- src/Platform/HTTP.hs
+++ src/Platform/HTTP.hs
@@ -15,9 +15,11 @@ import Faker
 import Faker.Address
 import Faker.Combinators
 import Faker.Name
+import GHC.List as L
 import GHC.Generics
 import System.Random
 import Web.Scotty
+
 
 newUUID :: [UUID]
 newUUID =
@@ -38,6 +40,9 @@ data User =
 instance ToJSON User
 instance FromJSON User
 
+matchesId :: String -> User -> Bool
+matchesId beam user = toString (userId user) == beam
+
 start :: Int -> IO ()
 start port = do
   dataName <- generateNonDeterministic $ listOf 3 name
@@ -45,10 +50,11 @@ start port = do
   let user1 = User (newUUID !! 0) (dataName !! 0) (dataAddress !! 0)
   let user2 = User (newUUID !! 1) (dataName !! 1) (dataAddress !! 1)
   let user3 = User (newUUID !! 2) (dataName !! 2) (dataAddress !! 2)
-  let allUsers = [user1, user2, user3]
+  let allUsers = [user1,user2, user3]
   print ("Starting Server at port " ++ show port)
   scotty port $ do
     get "/" $ html $ "<h1>Backend API server (haskell Scotty)</h1>"
+    get "/users/:word" $ do { beam <- captureParam "word" ; json (L.filter (matchesId beam) allUsers) }
     get "/manoa.json" $ json allUsers
     get "/florida.json" $ do
       json $ object ["top news" .= ("Hurricane Milton weakens as it marches across central Florida" :: Text), "sports" .= ("Why 49ers should watch former UH linebacker Jeff Ulbrich, Jets interim coach" :: Text)]
