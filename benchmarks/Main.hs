0109d6efeb9cadf8409c57af3e0e46b12c307dcd
Oleg G.Kapranov <ok@pure-agency.co>
2025-01-20 04:11:22 -1000

    updated benchmarks Main.hs

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -1,21 +1,35 @@
 {-# LANGUAGE QuasiQuotes #-}
 import Criterion.Main
-import Auth.Bisque
+import Auth.Bisque as B
+import Auth.Kailua as K
 
-buildToken :: SecretKey -> IO (Bisque Open Verified)
-buildToken sk = do
-  mkBisque sk [block|user_id("user_1234");|]
+buildTokenB :: B.SecretKey -> IO (B.Bisque B.Open B.Verified)
+buildTokenB sk = do
+  B.mkBisque sk [B.block|user_id("user_1234");|]
+
+buildTokenK :: K.SecretKey -> IO (K.Kailua K.Open K.Verified)
+buildTokenK sk = do
+  K.mkKailua sk [K.block|user_id("user_1234");|]
 
 main :: IO ()
 main = do
-  sk <- newSecret
-  bisque <- buildToken sk
-  let pk = toPublic sk
-  let bisqueBs = serialize bisque
+  skb <- B.newSecret
+  skk <- K.newSecret
+  bisque <- buildTokenB skb
+  kailua <- buildTokenK skk
+  let pkb = B.toPublic skb
+  let pkk = K.toPublic skk
+  let bisqueBs = B.serialize bisque
+  let kailuaBs = K.serialize kailua
   defaultMain [
-    bgroup "bisque" [ bench "mkBisque"  $ whnfIO (buildToken sk)
-                    , bench "parse"     $ whnf (parse pk) bisqueBs
-                    , bench "serialize" $ whnf serialize bisque
-                    , bench "verify"    $ whnfIO (authorizeBisque bisque [authorizer|allow if user_id("user_1234");|])
+    bgroup "bisque" [ bench "mkBisque"  $ whnfIO (buildTokenB skb)
+                    , bench "parse"     $ whnf (B.parse pkb) bisqueBs
+                    , bench "serialize" $ whnf B.serialize bisque
+                    , bench "verify"    $ whnfIO (B.authorizeBisque bisque [B.authorizer|allow if user_id("user_1234");|])
+                    ],
+    bgroup "kailua" [ bench "mkKailua"  $ whnfIO (buildTokenK skk)
+                    , bench "parse"     $ whnf (K.parse pkk) kailuaBs
+                    , bench "serialize" $ whnf K.serialize kailua
+                    , bench "verify"    $ whnfIO (K.authorizeKailua kailua [K.authorizer|allow if user_id("user_1234");|])
                     ]
     ]
